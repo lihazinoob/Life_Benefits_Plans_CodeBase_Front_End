07fed60231f2d3cb6a73abdda1fcae4a7f5cd752
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2025-02-12 01:21:19 +0600

    Responsiveness added

--- src/Layouts/LandingPage/LandingPage.jsx
+++ src/Layouts/LandingPage/LandingPage.jsx
@@ -9,25 +9,25 @@ import Option from "../../Components/Options/Option";
 const LandingPage = () => {
   return (
     <div className="font-Montserrat">
-      <div className="lg:px-24  py-1">
+      <div className="lg:px-24   py-1">
         <Header />
       </div>
-      <div className="lg:px-24 py-1">
+      <div className="lg:px-24 2xl:px-96 py-1">
         <Option />
       </div>
-      <div className="lg:px-24 py-1">
+      <div className="lg:px-24 2xl:px-96 py-1">
         <StateCard />
       </div>
-      <div className="lg:px-24 py-1">
+      <div className="lg:px-24 2xl:px-96 py-1">
         <ExpertOpinion/>
       </div>
-      <div className="lg:px-24 py-1">
+      <div className="lg:px-24 2xl:px-96 py-1">
         <FAQ/>
       </div>
-      <div className="lg:px-24 py-1">
+      <div className="lg:px-24 2xl:px-96 py-1">
         <LandingCard/>
       </div>
-      <div className="lg:px-24 py-1">
+      <div className="lg:px-24 2xl:px-96 py-1">
         <Footer />
       </div>
       <div className="lg:px-24 py-1"></div>
