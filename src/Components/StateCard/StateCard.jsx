86488a9adf457e7df2f8f6eedd83c952a0f97669
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2025-02-12 00:26:22 +0600

    The design may be finalized

--- src/Components/StateCard/StateCard.jsx
+++ src/Components/StateCard/StateCard.jsx
@@ -4,18 +4,16 @@ import { useNavigate } from "react-router-dom";
 const StateCard = () => {
   const navigate = useNavigate();
 
-  
-  const handleStateClick = (stateName) =>
-  {
-    // Saving the data to localstorage so that it doess not vanish 
-    localStorage.setItem("selectedState",stateName);
+  const handleStateClick = (stateName) => {
+    // Saving the data to localstorage so that it doess not vanish
+    localStorage.setItem("selectedState", stateName);
     // Navigate to the form page with the selected state
-    navigate("/form",{
-      state:{
-        selectedState:stateName
-      }
+    navigate("/form", {
+      state: {
+        selectedState: stateName,
+      },
     });
-  }
+  };
 
   return (
     <div className="w-full mt-16 py-6 px-4 md:px-8 bg-[#F6F8F7] rounded-lg">
@@ -28,15 +26,17 @@ const StateCard = () => {
       </div>
 
       {/* Grid Layout for Buttons */}
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-6 gap-y-2 my-12 mx-16 ">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-x-0.5 gap-y-2 my-12 text-center mx-auto max-w-screen-lg">
         {US_STATES.map(({ name }) => (
-          <button
-            key={name}
-            className="bg-inherit hover:bg-[#4970FA] text-black hover:text-white font-semibold py-4   rounded border-2 border-solid transition duration-200 text-xs "
-            onClick={() => handleStateClick(name)}
-          >
-            {name}
-          </button>
+          <div className="rounded border-2 border-solid text-center w-3/4 bg-inherit hover:bg-[#4970FA] text-black hover:text-white transition duration-200 text-xs cursor-pointer">
+            <button
+              key={name}
+              className="font-semibold py-3"
+              onClick={() => handleStateClick(name)}
+            >
+              {name}
+            </button>
+          </div>
         ))}
       </div>
     </div>
