735970367ab52926cad54ee6d9ed4897f66f0c72
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2025-03-03 18:25:51 +0600

    Fahad Monkey

--- src/Components/StateCard/StateCard.jsx
+++ src/Components/StateCard/StateCard.jsx
@@ -35,7 +35,7 @@ const StateCard = () => {
       <div className="block sm:hidden my-8 mx-auto  bg-[url('https://a-us.storyblok.com/f/1016477/350x210/bb37a90934/usa-map.webp')] bg-cover bg-center bg-no-repeat py-16">
         <select
           onChange={handleSelectChange}
-          className="w-[210px] max-w-xs mx-auto block px-4 py-3 rounded border-2 border-solid text-inherit font-semibold text-black bg-white focus:outline-none focus:border-[#4970FA]"
+          className="w-[210px] max-w-xs mx-auto block px-4 py-4 rounded border-2 border-solid text-inherit font-semibold text-black bg-white focus:outline-none focus:border-[#4970FA]"
           defaultValue=""
         >
           <option value="" disabled className="text-center">
